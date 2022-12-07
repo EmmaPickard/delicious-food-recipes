a430337ae799eb77f6b33febdbbb703bfea4a90d
Emma Pickard <emma.joannemarie21@gmail.com>
2022-12-06 19:59:23 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import React, { useState } from "react";
+import "./App.css"
+import RecipeCreate from "./api/RecipeCreate";
+import RecipeList from "./api/RecipeList";
+import RecipeData from "./api/RecipeData"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+ //create a use tate for new and preexisting recipe data 
+ const [recipes, setRecipes] = useState(RecipeData);
+
+  
+  
+  
+ //uses the form data and spreads it to the recipe array 
+ const addRecipe = (data) => {
+   setRecipes([...recipes, data])}
+
+//adds the delete functionality to each row containing a recipe
+function deleteRecipe(targetIndex) {
+setRecipes(recipes.filter((_, index) => index !== targetIndex))
+}
+// creating props so I can use addRecipe and deleteRecipe  in my components
+ return (
+   <div className="App">
+     <header><h1>Delicious Food Recipes</h1></header>
+     <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
+     <RecipeCreate addRecipe = {addRecipe}/>
+   </div>
+ );
 }
 
 export default MyApp
